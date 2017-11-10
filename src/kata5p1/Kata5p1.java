2a03e3e633648cb4ccec505b6dfa7b186d29e685
usuario <usuario@10.22.148.53>
2017-11-10 13:01:20 +0000

    Kata5p1, solo lo basico

---
+++ src/kata5p1/Kata5p1.java
@@ -0,0 +1,19 @@
+package kata5p1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Kata5p1 {
+
+    public static void main(String[] args) 
+            throws ClassNotFoundException, SQLException {
+        Class.forName("org.sqlite.JDBC");
+        Connection cm = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\usuario\\Downloads\\SQLiteDatabaseBrowserPortable\\Data\\Kata5.db");
+        Statement st = cm.createStatement();
+        ResultSet rs = st.executeQuery("select name from people");
+        
+    }
+}
